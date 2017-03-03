fe137bd074689272d55ed5a14c48a64b8fb6d2b3
JayShower <lacrossejason@gmail.com>
2017-03-03 00:34:06 -0600

    Structure of code actually set now

--- NetworkTableLogging/src/Logger.java
+++ NetworkTableLogging/src/Logger.java
@@ -2,36 +2,31 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class Logger {
 
-	// Final fields
-	private static final String tableKey = "SmartDashboard";
+	// Final/Helper fields
+	private static final String	ConfigFileName	= "Config";
+	private static final String	tableKey		= "SmartDashboard";
+	// private static final String networkIdentity = "networkIdentity";
+	private static boolean	wasConnected		= true;
+	private static boolean	waitingToConnect	= true;
 
-	// Config fields / depends on config fields
+	// Config fields / fields dependant on config fields
 	private static int			teamNumber;
 	private static String[]		keys;
 	private static NetworkTable	table;
-
-	// Helper fields
-	private static boolean	wasConnected		= true;
-	private static boolean	waitingToConnect	= true;
 
 	// Logging fields
 	// private static LogFile logfile = new LogFile();
 
 	public static void main(String[] args) {
+		System.out.println("Application starting");
 		readConfigFile();
 		initializeNetworkTableClient();
 
-		// wait for connection (or end if code is canceled)
-		while (!table.isConnected()) {
-			if (Thread.interrupted())
-				System.out.println("Program ending");
-			return;
-		}
+		while (!table.isConnected()) {}
 
 		initializeLogging();
 
-		// run until code is canceled
-		while (!Thread.interrupted()) {
+		while (true) {
 			if (!table.isConnected()) {
 				if (wasConnected)
 					afterDisconnected();
@@ -42,9 +37,6 @@ public class Logger {
 				whileConnected();
 			}
 		}
-
-		System.out.println("Program ending, saving current data");
-		saveLogFile();
 	}
 
 	/**
@@ -59,13 +51,11 @@ public class Logger {
 	 */
 	private static void initializeNetworkTableClient() {
 		System.out.println("Disconnected, initializing NetworkTable Client");
-
 		NetworkTable.setClientMode();
-		// NetworkTable.setNetworkIdentity("Name"); needed?
+		// NetworkTable.setNetworkIdentity(networkIdentity); needed?
 		NetworkTable.setTeam(teamNumber);
 		NetworkTable.initialize();
 		table = NetworkTable.getTable(tableKey);
-
 		// Print status
 		System.out.println("NetworkTable Client initialized");
 		System.out.println("Disconnected, logging not initialized");
@@ -77,10 +67,9 @@ public class Logger {
 	 */
 	private static void initializeLogging() {
 		System.out.println("Connected, initializing logging");
-
+		openNewLogFile();
 		waitingToConnect = false;
 		wasConnected = true;
-
 		System.out.println("Logging initialized");
 	}
 
@@ -89,7 +78,6 @@ public class Logger {
 	 */
 	private static void afterDisconnected() {
 		System.out.println("Disconnected");
-
 		wasConnected = false;
 		waitingToConnect = true;
 	}
@@ -99,20 +87,24 @@ public class Logger {
 	 */
 	private static void afterConnected() {
 		System.out.println("Connected");
-
-		saveLogFile();
-		newLogFile();
-
+		closeLogFile();
+		openNewLogFile();
 		waitingToConnect = false;
 		wasConnected = true;
 	}
 
 	private static void whileDisconnected() {}
 
-	private static void whileConnected() {}
+	private static void whileConnected() {
+		writeToLogFile();
+	}
 
-	private static void saveLogFile() {}
+	private static void closeLogFile() {}
 
-	private static void newLogFile() {}
+	private static void openNewLogFile() {}
+
+	private static void writeToLogFile() {
+
+	}
 
 }
